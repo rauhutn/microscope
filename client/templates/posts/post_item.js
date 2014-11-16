8824036354671c0b3284bd633785a29f3261a61e
moonfog <nico.rauhut@web.de>
2014-11-16 09:16:45 +0000

    editing/delting posts

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,4 +1,10 @@
 Template.postItem.helpers({
+  ownPost: function() {
+    console.log(this.userId);
+    console.log(Meteor.userId());
+    return this.userId === Meteor.userId();
+  },
+  
   domain: function() {
     var a = document.createElement('a');
     console.log(this);
